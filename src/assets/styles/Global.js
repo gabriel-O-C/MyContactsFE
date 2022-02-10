e6840f03b92db32d0631e02f13fd99adffbd9d00
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2022-02-10 17:48:37 +0000

    chore: global styles

---
+++ src/assets/styles/Global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Sora', sans-serif;
+  }
+  html{
+    font-size: 62.5%;
+  }
+  body{
+    font-size: 1.6rem;
+    background: ${({ theme }) => theme.backgroundColor};
+  }
+  button{
+    cursor: pointer;
+  }
+`;
