2b90f1284eb5ca8d1c0b011780405dd14f63741b
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2022-02-11 17:57:01 +0000

    feat: input and select components

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,20 @@
 import { PageHeader } from '../../components';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+      <Input />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
