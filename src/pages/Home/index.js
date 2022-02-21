82d0f71d9e227f884e41ef4fe3e72ff80f5c0748
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2022-02-21 22:53:46 +0000

    feat: implementation of search contacts

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import {
-  Card, Container, Header, ListContainer, InputSearchContainer,
+  Card, Container, Header, ListHeader, InputSearchContainer,
 } from './styles';
 import Arrow from '../../assets/images/icons/arrow.svg';
 import Edit from '../../assets/images/icons/edit.svg';
@@ -9,8 +9,13 @@ import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+  const filteredContacts = contacts.filter((contact) => (
+    contact.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+  ));
   useEffect(() => {
-    fetch('http://localhost:3001/contacts')
+    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
       .then(async (response) => {
         const json = await response.json();
         setContacts(json);
@@ -18,50 +23,63 @@ export default function ContactsList() {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [orderBy]);
+
+  function handleToggleOrderBy() {
+    setOrderBy(
+      (prevState) => (prevState === 'asc' ? 'desc' : 'asc'),
+    );
+  }
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
   return (
     <Container>
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar contato..." />
+        <input
+          onChange={handleChangeSearchTerm}
+          type="text"
+          placeholder="Pesquisar contato..."
+        />
       </InputSearchContainer>
       <Header>
         <strong>
-          {contacts.length}
-          {contacts.length === 1 ? ' contato' : ' contatos'}
+          {filteredContacts.length}
+          {filteredContacts.length === 1 ? ' contato' : ' contatos'}
         </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
-      <ListContainer>
-        <header>
-          <button type="button">
-            <span>nome</span>
-            <img src={Arrow} alt="arrow" />
-          </button>
-        </header>
-        {contacts.map((contact) => (
-          <Card key={contact.id}>
-            <div className="info">
-              <div className="contact-name">
-                <strong>{contact.name}</strong>
-                {contact.category_name && (
-                <small>instagram</small>
-                )}
-              </div>
-              <span>{contact.email}</span>
-              <span>{contact.phone}</span>
-            </div>
-            <div className="actions">
-              <Link to={`/edit/${contact.id}`} alt="edit contacts page">
-                <img src={Edit} alt="edit" />
-              </Link>
-              <button type="button">
-                <img src={trash} alt="delete" />
-              </button>
-            </div>
-          </Card>
-        ))}
+      {filteredContacts.length > 0 && (
+      <ListHeader orderBy={orderBy}>
+        <button type="button" onClick={handleToggleOrderBy}>
+          <span>nome</span>
+          <img src={Arrow} alt="arrow" />
+        </button>
 
-      </ListContainer>
+      </ListHeader>
+      )}
+      {filteredContacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
+              {contact.category_name && (
+                <small>{contact.category_name}</small>
+              )}
+            </div>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
+          </div>
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`} alt="edit contacts page">
+              <img src={Edit} alt="edit" />
+            </Link>
+            <button type="button">
+              <img src={trash} alt="delete" />
+            </button>
+          </div>
+        </Card>
+      ))}
     </Container>
   );
 }
