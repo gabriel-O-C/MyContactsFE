cd29675b8de58c8ad68128c81f1df92c7dccd208
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-05-06 17:27:16 -0300

    feat: add post verb to httpClient

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -21,6 +21,30 @@ class HttpClient {
 
     throw new APIError(response, body);
   }
+
+  async post(path, body) {
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+    });
+    const response = await fetch(`${this.baseURL}${path}`, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(body),
+    });
+    const contentType = response.headers.get('Content-Type');
+
+    let responseBody = null;
+
+    if (contentType.includes('application/json')) {
+      responseBody = response.json();
+    }
+
+    if (response.ok) {
+      return responseBody;
+    }
+
+    throw new APIError(response, responseBody);
+  }
 }
 
 export default HttpClient;
