f2c8555b4ee6d72a6bbb692f3608e298987909c0
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-05-21 19:12:39 -0300

    feat: add ToastContainer component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,12 +5,14 @@ import defaultTheme from '../../assets/styles/themes/default';
 import { Container } from './styles';
 import { Header } from '../index';
 import AppRoutes from '../../Routes/AppRoutes';
+import ToastContainer from '../Toast/ToastContainer';
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyles />
+        <ToastContainer />
         <Container>
           <Header />
           <AppRoutes />
---
+++ src/components/Toast/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: fixed;
+  left: 50%;
+  bottom: 40px;
+  z-index: 2;
+  transform: translateX(-50%);
+`;
