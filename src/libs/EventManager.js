80be0a84f841d2140ac44b928998fcfe7e732a0e
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-07-04 21:11:35 -0300

    refactor: use Map instead of plain object

--- src/libs/EventManager.js
+++ src/libs/EventManager.js
@@ -1,32 +1,31 @@
 export default class EventManager {
   constructor() {
-    this.listeners = {};
+    this.listeners = new Map();
   }
 
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, [])
     }
 
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
-    const listeners = this.listeners[event]
-    if (!listeners) return;
+    if (!this.listeners.has(event)) return;
 
-    listeners.forEach((listener) => {
-      listener(payload);
-    });
+    this.listeners.get(event).foreach((listener) => {
+      listener(payload)
+    })
   }
 
   removeListener(event, listenerToRemove) {
-    const listeners = this.listeners[event]
+    const listeners = this.listeners.get(event)
     if (!listeners) return;
 
-    const filteredListeners = listeners.filter((listener) => listener !== listenerToRemove)
+    const filteredListeners = listeners.filter((listener) => listener !== listenerToRemove);
 
-    this.listeners = filteredListeners
+    this.listeners.set(event, filteredListeners)
   }
 }
 
