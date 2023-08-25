c186385fa455a8cf6b07ea28feb6bde11d8cfca9
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-08-24 21:57:07 -0300

    ✨ redirect user if contact is not found

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,13 +1,40 @@
-import { PageHeader } from '../../components';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Loader, PageHeader } from '../../components';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(1);
+        console.log(contactData);
+        setIsLoading(false);
+      } catch {
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado',
+        });
+        // TODO implement a solution using react-router-dom
+        window.location.assign('/');
+      }
+    }
+    loadContact();
+  }, [id]);
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader
         title="Editar contato"
       />
       <ContactForm
+        onSubmit={() => {}}
+        key={Math.random()}
         buttonLabel="Salvar alterações"
       />
     </>
