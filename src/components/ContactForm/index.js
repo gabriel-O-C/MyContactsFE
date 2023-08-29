d90b40de64f64210f70f077c060be88a93bb7361
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-08-29 15:58:25 -0300

    refactor: clear form fields on create contact

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -56,6 +56,12 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
       setPhone(formatPhone(contact.phone) ?? '');
       setCategoryID(contact.category_id ?? '');
     },
+    resetFields: () => {
+      setName('');
+      setEmail('');
+      setPhone('');
+      setCategoryID('');
+    },
   }), []);
 
   useEffect(() => {
--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { PageHeader } from '../../components';
 import ContactForm from '../../components/ContactForm';
 import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
 
 export default function NewContact() {
+  const contactFormRef = useRef(null);
   /**
    *
    * @param {{name: string, email: string, phone: string, categoryID: string}} formData
@@ -24,6 +25,8 @@ export default function NewContact() {
         type: 'success',
         text: 'Contato cadastrado com sucesso!',
       });
+
+      contactFormRef.current.resetFields();
     } catch {
       toast({
         type: 'danger',
@@ -37,6 +40,7 @@ export default function NewContact() {
         title="Novo contato"
       />
       <ContactForm
+        ref={contactFormRef}
         buttonLabel="Cadastrar"
         onSubmit={handleSubmit}
       />
